d75226e4385ab5bc3282d2b1d08d470b2fdb9adf
utahta <labs.ninxit@gmail.com>
2017-08-20 18:20:09 +0900

    Update keymap for windows

--- keyboards/ergodox/keymaps/utahta/keymap.c
+++ keyboards/ergodox/keymaps/utahta/keymap.c
@@ -103,18 +103,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 2: Windows layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * | ESC    |   1  |   2  |   3  |   4  |   5  |      |           |      |   6  |   7  |   8  |   9  |   0  |  BS    |
+ * | ESC    |   1  |   2  |   3  |   4  |   5  |  ;   |           |      |   6  |   7  |   8  |   9  |   0  | BS/DEL |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * | Tab    |   Q  |   W  |   E  |   R  |   T  |  `   |           |  '   |   Y  |   U  |   I  |   O  |   P  |   \    |
+ * | Tab    |   Q  |   W  |   E  |   R  |   T  |  ^   |           |  *   |   Y  |   U  |   I  |   O  |   P  |   ¥    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | LCtrl  |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |   -    |
  * |--------+------+------+------+------+------|  [   |           |  ]   |------+------+------+------+------+--------|
- * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  |   =    |
+ * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  |   \    |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   | LCtrl| LAlt |      | ZKHK | LCtrl|                                       | Left | Down |  Up  | Right|   L1 |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,---------------.
- *                                        | LAlt | L3   |       | Left | Right  |
+ *                                        | LAlt | TGL  |       | Left | Right  |
  *                                 ,------|------|------|       |------+--------+------.
  *                                 |      |      | Home |       | UP   |        |      |
  *                                 | Space|LShift|------|       |------| BS     | Enter|
@@ -125,19 +125,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
 [WINS] = KEYMAP(  // layer 0 : default
         // left hand
-        KC_ESC,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_NO,
-        KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   KC_GRV,
+        KC_ESC,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_SCLN,
+        KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   KC_EQL,
         KC_LCTRL,       KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
-        KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   KC_LBRC,
+        KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   KC_RBRC,
         KC_LCTRL, KC_LALT, KC_NO, KC_ZKHK, KC_LCTRL,
                                                              KC_LALT, KC_TRNS,
                                                                       KC_HOME,
                                                      KC_SPC, KC_LSFT, KC_END,
         // right hand
-        KC_NO,       KC_6,   KC_7,  KC_8,   KC_9,   KC_0,             KC_BSPC,
-        KC_QUOT,     KC_Y,   KC_U,  KC_I,   KC_O,   KC_P,             KC_BSLS,
+        KC_NO,       KC_6,   KC_7,  KC_8,   KC_9,   KC_0,             MT(KC_DEL, KC_BSPC),
+        KC_QUOT,     KC_Y,   KC_U,  KC_I,   KC_O,   KC_P,             KC_JYEN,
                      KC_H,   KC_J,  KC_K,   KC_L,   KC_SCLN,          KC_MINS,
-        KC_RBRC,     KC_N,   KC_M,  KC_COMM,KC_DOT, KC_SLSH,          KC_EQL,
+        KC_BSLS,     KC_N,   KC_M,  KC_COMM,KC_DOT, KC_SLSH,          KC_RO,
                                  KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, TG(SYMB),
         KC_LEFT, KC_RGHT,
         KC_UP,
