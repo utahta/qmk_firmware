d381534edf5338f2910d597b51440ce585b7f807
utahta <labs.ninxit@gmail.com>
2017-08-21 18:16:02 +0900

    Update keymap for windows

--- keyboards/ergodox/keymaps/utahta/keymap.c
+++ keyboards/ergodox/keymaps/utahta/keymap.c
@@ -110,7 +110,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|  [   |           |  ]   |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  |   \    |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   | LCtrl| LAlt |      | ZKHK | LCtrl|                                       | Left | Down |  Up  | Right|   L1 |
+ *   | LCtrl|  Win |      | ZKHK | LAlt |                                       | Left | Down |  Up  | Right|   L1 |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,---------------.
  *                                        | LAlt | TGL  |       | Left | Right  |
@@ -128,7 +128,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   KC_EQL,
         KC_LCTRL,       KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   KC_RBRC,
-        KC_LCTRL, KC_LALT, KC_NO, KC_ZKHK, KC_LCTRL,
+        KC_LCTRL, KC_LGUI, KC_NO, KC_ZKHK, KC_LALT,
                                                              KC_LALT, KC_TRNS,
                                                                       KC_HOME,
                                                      KC_SPC, KC_LSFT, KC_END,
